e9d36b684e26a4c7007afc791ade64d254957f27
Alejandro Larumbe <larumbemar@gmail.com>
2020-11-01 15:38:56 +1300

    photo

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 module.exports = {
   baseUrl:
-  process.env.NODE_ENV === "development"
-    ? 'http://localhost:8000/api'
-    : "https://jamupjammer.herokuapp.com/api",
-  imageUrl: process.env.PUBLIC_URL + '/images',
+    process.env.NODE_ENV === "development"
+      ? 'http://localhost:8000/api'
+      : "https://jamupjammer.herokuapp.com/api",
   // export const baseUrl = process.env.REACT_APP_BASEURL || 'http://localhost:8000/api';
+  imageUrl:
+    process.env.NODE_ENV === "development"
+      ? 's3://jamup'
+      : process.env.PUBLIC_URL + '/images',
 }
