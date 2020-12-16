b7e40e95d5861854af386d3f993f8edcc234eaf3
Alejandro Larumbe <larumbemar@gmail.com>
2020-12-17 06:08:14 +1300

    removed avatar group

--- src/components/CityJamsCard.js
+++ src/components/CityJamsCard.js
@@ -3,7 +3,6 @@ import { useLocation, useHistory, useParams } from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import Avatar from '@material-ui/core/Avatar';
 import { formatTime } from './utils';
-import AvatarGroup from '@material-ui/lab/AvatarGroup';
 
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -11,7 +10,6 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CardHeader from '@material-ui/core/CardHeader';
